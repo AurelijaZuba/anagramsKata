d027bac1ee79a6df5e29599a5ca45199e757714e
AurelijaZuba <auryte00@hotmail.com>
2019-06-27 14:18:16 +0100

    ...

--- src/test/java/com/codurance/AnagramShould.java
+++ src/test/java/com/codurance/AnagramShould.java
@@ -1,6 +1,29 @@
 package com.codurance;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 public class AnagramShould {
 
 
+//    Write a program to generate all potential anagrams of an input string.
+//    For example, the potential anagrams of "biro" are
+//    biro bior brio broi boir bori
+//    ibro ibor irbo irob iobr iorb
+//    rbio rboi ribo riob roib robi
+//    obir obri oibr oirb orbi orib
+
+    @Test
+    void acceptance_test() {
+        String[] expected = [  "biro",  "bior",  "brio",  "broi", boir bori
+        ibro ibor irbo irob iobr iorb
+        rbio rboi ribo riob roib robi
+        obir obri oibr oirb orbi orib];
+
+        String[] results = Anagram.generate("biro");
+        assertArrayEquals(expected, results);
+    }
 }
