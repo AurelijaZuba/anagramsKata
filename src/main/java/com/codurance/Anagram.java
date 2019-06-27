a3c30533466a253ef95605f2ea21ab3b0b1c2225
AurelijaZuba <auryte00@hotmail.com>
2019-06-27 14:27:19 +0100

    Failing acceptance test for anagram generator

---
+++ src/main/java/com/codurance/Anagram.java
@@ -0,0 +1,7 @@
+package com.codurance;
+
+public class Anagram {
+    public static String[] generate(String input) {
+        throw new UnsupportedOperationException();
+    }
+}
--- src/test/java/com/codurance/AnagramShould.java
+++ src/test/java/com/codurance/AnagramShould.java
@@ -18,7 +18,11 @@ public class AnagramShould {
 
     @Test
     void acceptance_test() {
-        String[] expected = {"biro", "bior", "brio", "broi", "boir", "bori", "ibro", "ibor", "irbo", "irob", "iobr", "iorb", "rbio", "rboi", "ribo", "riob", "roib", "robi", "obir", "obri", "oibr", "oirb", "orbi", "orib"};
+        String[] expected = {"biro", "bior", "brio", "broi", "boir", "bori",
+                "ibro", "ibor", "irbo", "irob", "iobr", "iorb",
+                "rbio", "rboi", "ribo", "riob", "roib", "robi",
+                "obir", "obri", "oibr", "oirb", "orbi", "orib"};
+
 
         String[] results = Anagram.generate("biro");
         assertArrayEquals(expected, results);
