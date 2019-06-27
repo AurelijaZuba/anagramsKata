2d62e7d9d390f634087839229718d4aedea102c9
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-06-27 14:47:13 +0100

    Return two variations for two letter word ab

--- src/main/java/com/codurance/Anagram.java
+++ src/main/java/com/codurance/Anagram.java
@@ -1,8 +1,17 @@
 package com.codurance;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Anagram {
     public static String[] generate(String input) {
-        String[] output = { input };
-        return output;
+        List<String> output = new ArrayList<>();
+
+        output.add(input);
+
+        if(input.length() > 1)
+            output.add("ba");
+
+        return output.toArray(new String[0]);
     }
 }
